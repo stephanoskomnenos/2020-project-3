c7ad0d4ad7453ceff72842041a3886b72108e047
Stephanos Komnenos <ljt267@126.com>
2020-10-07 23:15:40 +0800

    Add files via upload

--- ver01.hs
+++ ver01.hs
@@ -1,7 +1,6 @@
 import qualified Data.Set as S
 
 data Foo = Foo { left :: [Foo], right :: [Foo] }
-    deriving (Show)
     
 instance Ord Foo where
     lfo <= rfo = (not (S.member True (S.fromList (map (rfo <=) (left lfo))))) && (not(S.member True (S.fromList (map (<= lfo) (right rfo)))))                    
@@ -11,3 +10,7 @@ instance Eq Foo where
 
 instance Num Foo where
     (+) lfo rfo = Foo (S.toList (S.union (S.fromList (map (lfo +) (left rfo))) (S.fromList (map (rfo +) (left lfo))))) (S.toList (S.union (S.fromList (map (rfo +) (right lfo))) (S.fromList (map (lfo +) (right rfo)))))
+
+instance Show Foo where --Show这里还有很多问题
+    show (Foo lfo rfo) = show ("{" ++ init(tail(show lfo)) ++ "|" ++ init(tail(show rfo)) ++ "}")
+    
